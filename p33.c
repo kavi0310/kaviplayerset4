4c9d673a22a3b3479136a34c901c8a9fe16dbb39
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-03-13 19:23:10 +0530

    Create p33.c

---
+++ p33.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100][100],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+        scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+        if(i==j)
+        {
+            if(a[i][j]==1)
+            {
+            c++;
+            }
+        }
+        }
+    }
+    if(n!=1)
+    {
+    if(a[i][n-1]==1)
+    {
+        c++;
+    }
+    if(a[n-1][0]==1)
+    {
+    c++;
+    }
+    }
+    printf("%d",c);
+}
